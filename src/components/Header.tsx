387c1ba0db9de53f4f23184e8d7886500f66890d
AntiMatterZX <varunbhole@gmail.com>
2025-05-15 13:00:10 +0530

    Links Updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { FizziLogo } from "@/components/FizziLogo";
+import { BrewyLogo } from "@/components/BrewyLogo";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
     <header className="-mb-28 flex justify-center py-4">
-      <FizziLogo className="z-10 h-20 cursor-pointer text-sky-800" />
+      <BrewyLogo className="z-10 h-20 cursor-pointer text-sky-800" />
     </header>
   );
 }
